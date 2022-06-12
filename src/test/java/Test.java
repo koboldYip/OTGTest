6c29079bed1ef7572124aee9e804d7b1aebd9219
koboldYip <kyldoks@gmail.com>
2022-06-12 22:46:42 +0300

    Second seems good

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -13,4 +13,23 @@ public class Test {
         xlsxWriter.writeXLSX(table);
         System.out.println();
     }
+
+    @org.junit.jupiter.api.Test
+    public void test1() {
+        String text = "Hello!";
+        String time = "0 12 24 48 128 129";
+        int lengthText = text.length();
+        int maxTime = 0;
+        int maxIndex = 0;
+        String[] timeSplited = time.split(" ");
+
+        for (int i = 1; i < timeSplited.length; i++) {
+            if (Integer.parseInt(timeSplited[i]) - Integer.parseInt(timeSplited[i - 1]) > maxTime) {
+                maxTime = Integer.parseInt(timeSplited[i]);
+                maxIndex = i;
+            }
+        }
+        String[] textSplited = text.split("");
+        System.out.println("textSplited[maxIndex] = " + text.split("")[maxIndex]);
+    }
 }
